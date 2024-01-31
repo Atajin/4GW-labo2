66d52ec3e91abe5456afa2027d9c17b472cbfd92
Kian Chowanietz <kianlettres@gmail.com>
2024-01-31 09:19:20 -0500

    Init

---
+++ Projet_etudiant1/server.js
@@ -0,0 +1,21 @@
+/*
+    Importation des modules requis
+*/
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import mysql from "mysql";
+import { body, validationResult } from "express-validator";
+import dateFormat from "dateformat";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/*
+    Connect to server
+*/
+const server = app.listen(4000, function () {
+    console.log("serveur fonctionne sur 4000... ! ");
+});
